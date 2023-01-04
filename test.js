3155f9b88075a1a95befccdd38de3d0ba9dea7b4
jowallace1 <john.nagib.wallace@gmail.com>
2023-01-03 19:50:55 -0800

    Removed test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-function calculateDaysBetweenDates(begin, end) {
-  const beginDate = new Date(begin);
-  const endDate = new Date(end);
-  const diff = endDate.getTime() - beginDate.getTime();
-  return diff / 1000 / 60 / 60 / 24;
-}
